f76f93ff0beabd22364b303b85243a7b2389173b
imamsutono <imamssutono@gmail.com>
2017-11-26 08:20:00 +0700

    change from listview to flatlist

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import { ListView } from 'react-native';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import ListItem from './ListItem';
 
 class LibraryList extends Component {
-  componentWillMount() {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(this.props.libraries);
-  }
-
-  renderRow(library) {
-    return <ListItem library={library} />;
-  }
+  renderItem = ({ item }) => (
+    <ListItem library={item} />
+  );
 
   render() {
+    const { libraries } = this.props;
+
     return (
-      <ListView
-        dataSource={this.dataSource}
-        renderRow={this.renderRow}
+      <FlatList
+        data={libraries}
+        keyExtractor={item => item.id}
+        renderItem={this.renderItem}
       />
     );
   }
